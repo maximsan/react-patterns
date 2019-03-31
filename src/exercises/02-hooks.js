3192817ae3507772c4f1d0e190698655c5f56bbf
maximsan <maximuzz@yandex.ru>
2019-04-01 00:51:16 +0300

    Add 2 solution with hooks

---
+++ src/exercises/02-hooks.js
@@ -0,0 +1,34 @@
+// Compound Components
+
+import React, {useState} from 'react';
+import {Switch} from '../switch';
+
+const Toggle = ({onToggle, children}) => {
+  const [on, setOn] = useState(false);
+  const toggle = () => {
+    setOn(!on);
+    onToggle(!on);
+  };
+
+  return React.Children.map(children, child => 
+    React.cloneElement(child, {on, toggle}));
+};
+
+Toggle.On = ({on, children}) => <>{on ? children : null}</>;
+Toggle.Off = ({on, children}) => <>{!on ? children : null}</>;
+Toggle.Button = ({on, toggle, ...props}) => (
+  <Switch onClick={toggle} on={on} {...props} />
+);
+
+function Usage() {
+  return (
+    <Toggle onToggle={(...args) => console.log('onToggle', ...args)}>
+      <Toggle.On>The button is on</Toggle.On>
+      <Toggle.Off>The button is off</Toggle.Off>
+      <Toggle.Button />
+    </Toggle>
+  );
+}
+Usage.title = 'Compound Components';
+
+export {Toggle, Usage as default};
