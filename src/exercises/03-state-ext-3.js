981a28dca64164d8785f0e5ffafa4e1c966a7ea1
maximsan <maximuzz@yandex.ru>
2019-04-01 23:03:22 +0300

    Add 3 solution with class plus to improve rendering on update

---
+++ src/exercises/03-state-ext-3.js
@@ -0,0 +1,74 @@
+// Flexible Compound Components with context
+
+import React from 'react';
+import {Switch} from '../switch';
+
+const ToggleContext = React.createContext();
+
+const ToggleConsumer = props => {
+    <ToggleContext.Consumer>
+        {context => {
+            if(!context)
+                throw new Error(`Toggle should be within the toggle context`);
+            return props.children(context);
+        }}
+    </ToggleContext.Consumer>
+}
+
+class Toggle extends React.Component {
+  static On = ({children}) => (
+    <ToggleConsumer>
+      {value => (value.on ? children : null)}
+    </ToggleConsumer>
+  );
+
+  static Off = ({children}) => (
+    <ToggleConsumer>
+      {value => (!value.on ? children : null)}
+    </ToggleConsumer>
+  );
+
+  static Button = props => (
+    <ToggleConsumer>
+      {value => (
+        <Switch on={value.on} onClick={value.toggle} {...props} />
+      )}
+    </ToggleConsumer>
+  );
+
+  toggle = () =>
+    this.setState(
+      ({on}) => ({on: !on}),
+      () => this.props.onToggle(this.state.on),
+    );
+
+  //add toggle to state to initiate rerender only when 'on' in state will be updated
+  state = {on: false, toggle: this.toggle};
+
+  render() {
+    return (
+      <ToggleContext.Provider
+        value={this.state}
+      >
+        {this.props.children}
+      </ToggleContext.Provider>
+    );
+  }
+}
+
+function Usage({
+  onToggle = (...args) => console.log('onToggle', ...args),
+}) {
+  return (
+    <Toggle onToggle={onToggle}>
+      <Toggle.On>The button is on</Toggle.On>
+      <Toggle.Off>The button is off</Toggle.Off>
+      <div>
+        <Toggle.Button />
+      </div>
+    </Toggle>
+  );
+}
+Usage.title = 'Flexible Compound Components';
+
+export {Toggle, Usage as default};
