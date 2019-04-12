7aa224013db57f4794cbda615f0f84815b0f6f8a
maximsan <maximuzz@yandex.ru>
2019-04-12 09:18:00 +0300

    Add 06 solution with hooks

---
+++ src/exercises/06-hooks.js
@@ -0,0 +1,53 @@
+// prop getters
+
+import React, {useState} from 'react';
+import {Switch} from '../switch';
+
+const noop = () => {}
+
+const useToggle = ({onToggle = noop} = {}) => {
+  const [on, setOn] = useState(false);
+  const toggle = () => {
+    setOn(!on);
+    onToggle(!on);
+  };
+  const getTogglerProps = ({onClick, ...props}) => ({
+    'aria-pressed': on,
+    onClick: (...args) => {
+      onClick && onClick(...args);
+      toggle();
+    },
+    ...props,
+  });
+
+    return {
+      on,
+      toggle,
+      getTogglerProps,
+    };
+
+};
+
+function Usage() {
+    const {on, getTogglerProps} = useToggle({
+      onToggle: (...args) => console.log('onToggle', ...args),
+    })
+    return (
+      <div>
+        <Switch {...getTogglerProps({on})} />
+        <hr />
+        <button
+          {...getTogglerProps({
+            'aria-label': 'custom-button',
+            onClick: () => console.log('onButtonClick'),
+            id: 'custom-button-id',
+          })}
+        >
+          {on ? 'on' : 'off'}
+        </button>
+      </div>
+    )
+  }
+Usage.title = 'Prop Collections';
+
+export {Toggle, Usage as default};
