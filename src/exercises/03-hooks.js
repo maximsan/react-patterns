440c8cc3c51afbffcc6abb9ed2b571ea138feb23
maximsan <maximuzz@yandex.ru>
2019-10-20 00:17:41 +0300

    4 solution with hook API - add useCallback

--- src/exercises/03-hooks.js
+++ src/exercises/03-hooks.js
@@ -1,64 +1,64 @@
 // Flexible Compound Components with context
+
 import React, {
-    useState,
-    useContext,
-    useCallback,
-    useMemo,
-  } from 'react';
-  import {Switch} from '../switch';
-  
-  const ToggleContext = React.createContext();
-  
-  function useToggleContext() {
-    const toggleContext = useContext(ToggleContext);
-    if (!toggleContext)
-      throw new Error('Toggle should be within the toggle context');
-    return toggleContext;
-  }
-  
-  const Toggle = ({onToggle, ...rest}) => {
-    const [on, setOn] = useState(false);
-  
-    const toggle = useCallback(() => {
-      setOn(!on);
-      onToggle(!on);
-    }, [onToggle, on]);
-  
-    const value = useMemo(() => ({on: on, toggle: toggle}), [
-      on,
-      toggle,
-    ]);
-  
-    return <ToggleContext.Provider value={value} {...rest} />;
-  };
-  
-  Toggle.On = function On({children}) {
-    const {on} = useToggleContext();
-    return on ? children : null;
-  };
-  
-  Toggle.Off = function Off({children}) {
-    const {on} = useToggleContext();
-    return !on ? children : null;
-  };
-  
-  Toggle.Button = function Button(props) {
-    const {on, toggle} = useToggleContext();
-    return <Switch on={on} onClick={toggle} {...props} />;
-  };
-  
-  function Usage() {
-    return (
-      <div onToggle={(...args) => console.log('onToggle', ...args)}>
-        <Toggle.On>The button is on</Toggle.On>
-        <Toggle.Off>The button is off</Toggle.Off>
-        <div>
-          <Toggle.Button />
-        </div>
+  useState,
+  useContext,
+  useCallback,
+  useMemo,
+} from 'react';
+import {Switch} from '../switch';
+
+const ToggleContext = React.createContext();
+
+function useToggleContext() {
+  const toggleContext = useContext(ToggleContext);
+  if (!toggleContext)
+    throw new Error('Toggle should be within the toggle context');
+  return toggleContext;
+}
+
+const Toggle = ({onToggle, ...rest}) => {
+  const [on, setOn] = useState(false);
+
+  const toggle = useCallback(() => {
+    setOn(!on);
+    onToggle(!on);
+  }, [onToggle, on]);
+
+  const value = useMemo(() => ({on: on, toggle: toggle}), [
+    on,
+    toggle,
+  ]);
+
+  return <ToggleContext.Provider value={value} {...rest} />;
+};
+
+Toggle.On = function On({children}) {
+  const {on} = useToggleContext();
+  return on ? children : null;
+};
+
+Toggle.Off = function Off({children}) {
+  const {on} = useToggleContext();
+  return !on ? children : null;
+};
+
+Toggle.Button = function Button(props) {
+  const {on, toggle} = useToggleContext();
+  return <Switch on={on} onClick={toggle} {...props} />;
+};
+
+function Usage() {
+  return (
+    <div onToggle={(...args) => console.log('onToggle', ...args)}>
+      <Toggle.On>The button is on</Toggle.On>
+      <Toggle.Off>The button is off</Toggle.Off>
+      <div>
+        <Toggle.Button />
       </div>
-    );
-  }
-  Usage.title = 'Flexible Compound Components';
-  
-  export {Toggle, Usage as default};
-  
+    </div>
+  );
+}
+Usage.title = 'Flexible Compound Components';
+
+export {Toggle, Usage as default};
--- src/exercises/04-hooks.js
+++ src/exercises/04-hooks.js
@@ -1,16 +1,16 @@
 // custom hooks
 
-import React, {useState} from 'react';
+import React, {useState, useCallback} from 'react';
 import {Switch} from '../switch';
 
 const noop = () => {};
 const useToggle = ({onToggle = noop} = {}) => {
   const [on, setOn] = useState(false);
 
-  const toggle = () => {
+  const toggle = useCallback(() => {
     setOn(!on);
     onToggle(!on);
-  };
+  }, [on, onToggle]);
 
   return [on, toggle];
 };
