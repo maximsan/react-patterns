306ba4871ca4157fdcd4b739ffab861e6fa1ab8d
maximsan <maximuzz@yandex.ru>
2019-10-20 00:14:02 +0300

    1 solution with hook API - add useCallback

--- src/exercises/01-hooks.js
+++ src/exercises/01-hooks.js
@@ -1,22 +1,26 @@
 // Building the toggle component
 
-import React, {useState} from 'react'
-import {Switch} from '../switch'
+import React, {useState, useCallback} from 'react';
+import {Switch} from '../switch';
 
 const Toggle = ({onToggle}) => {
-  const [on, setOn] = useState(false)
-  const onHandleToggle = () => {
+  const [on, setOn] = useState(false);
+  const onHandleToggle = useCallback(() => {
     setOn(!on);
     onToggle(!on);
-  }
+  }, [on, onToggle]);
 
-  return <Switch on={on} onClick={onHandleToggle} />
-}
+  return <Switch on={on} onClick={onHandleToggle} />;
+};
 
 function Usage() {
-  return <Toggle onToggle={(...args) => console.log('onToggle', ...args)} />
+  return (
+    <Toggle
+      onToggle={(...args) => console.log('onToggle', ...args)}
+    />
+  );
 }
 
-Usage.title = 'Build Toggle'
+Usage.title = 'Build Toggle';
 
 export default Usage;
